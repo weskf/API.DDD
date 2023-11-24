262d3c485f2e8a9d50dbe6a0fb29c8a8e3dc41ce
Wescley K. Frohlich <wescley.kf@gmail.com>
2023-11-24 08:28:21 -0300

    UserService

---
+++ src/API.Domain/Interfaces/Services/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using API.Domain.Entidades;
+
+namespace API.Domain.Interfaces.Services.Users
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll();
+        Task<UserEntity> Post(UserEntity user);
+        Task<UserEntity> Put(UserEntity user);
+        Task<bool> Delete(Guid id);
+
+    }
+}
---
+++ src/API.Service/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using API.Domain.Entidades;
+using API.Domain.Interfaces;
+using API.Domain.Interfaces.Services.Users;
+
+namespace API.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private IRepository<UserEntity> _respository;
+
+        public UserService(IRepository<UserEntity> repository)
+        {
+            this._respository = repository;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _respository.DeleteAsync(id);  
+        }
+
+        public async Task<UserEntity> Get(Guid id)
+        {
+            return await _respository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            return await _respository.SelectAsync();
+        }
+
+        public async Task<UserEntity> Post(UserEntity user)
+        {
+            return await _respository.InsertAsync(user);
+        }
+
+        public async Task<UserEntity> Put(UserEntity user)
+        {
+            return await _respository.UpdateAsync(user);
+        }
+    }
+}
